597cf2fda354b6fbb1f5caa0ec25bd207b91ace5
Priyosmita <daspriyosmita2003@gmail.com>
2025-07-01 17:34:09 +0530

    add new student

--- app/components/Window/AddNewStudent.js
+++ app/components/Window/AddNewStudent.js
@@ -3,6 +3,10 @@ import React from 'react'
 const AddNewStudent = () => {
   return (
     <>
+      <div className="bg-white shadow-2xl h-fit w-full rounded-lg p-10">
+        <div className="text-2xl font-semibold text-gray-700 pb-3">Add New Student</div>
+        <hr className="font-bold border-1 text-gray-200" />
+      </div>
     </>
   )
 }

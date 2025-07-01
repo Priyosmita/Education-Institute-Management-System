aeb063780165de519c760a56320cfd58ce10a6f0
Priyosmita <daspriyosmita2003@gmail.com>
2025-07-01 17:41:56 +0530

    Recieve Fees struct

--- app/components/Window/RecieveFees.js
+++ app/components/Window/RecieveFees.js
@@ -3,6 +3,10 @@ import React from 'react'
 const RecieveFees = () => {
   return (
     <>
+      <div className="bg-white shadow-2xl h-fit w-full rounded-lg p-10">
+        <div className="text-2xl font-semibold text-gray-700 pb-3">Recieve Fees</div>
+        <hr className="font-bold border-1 text-gray-200" />
+      </div>
     </>
   )
 }

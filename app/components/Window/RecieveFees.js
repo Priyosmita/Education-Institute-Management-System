5b8c0e89f5b9bb0c269cf92cb771d6472ff0e9ba
Priyosmita <daspriyosmita2003@gmail.com>
2025-07-04 01:10:15 +0530

    fee buikding

--- app/components/Window/RecieveFees.js
+++ app/components/Window/RecieveFees.js
@@ -6,7 +6,12 @@ const RecieveFees = () => {
     <>
       <div className="bg-white shadow-2xl h-fit w-full rounded-lg p-10">
         <div className="text-2xl font-semibold text-gray-700 pb-3">Recieve Fees</div>
-        <hr className="font-bold border-1 text-gray-200" />
+        <hr className="font-bold border-1 text-gray-200 mb-7" />
+
+        <div className='rounded-lg p-7 bg-blue-50 shadow-xl'>
+          <p className='text-gray-700 text-xl font-semibold mb-4'>Student Fee Details</p>
+
+        </div>
       </div>
     </>
   )
